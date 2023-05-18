1ab24e6451fdceaa3ed65f495c8dba6ddb851fd2
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2023-05-18 10:38:19 +0530

    #include<stdio.h> int main() {     int a,i,c=0,p,avrg,d=0,j,k;     scanf("%d",&a);     int x[a];     for(i=0;i<a;i++)     {         scanf("%d",&x[i]);     }     int cx=0;      for(i=0;i<a;i++)     {         cx=cx+x[i];
        }
        printf("%d",cx);
       
    }

---
+++ Sum_of_Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,c=0,p,avrg,d=0,j,k;
+    scanf("%d",&a);
+    int x[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    int cx=0;
+     for(i=0;i<a;i++)
+    {
+        cx=cx+x[i];
+        
+    }
+    printf("%d",cx);
+   
+} 
